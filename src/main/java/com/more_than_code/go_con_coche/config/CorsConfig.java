a602906a38677eec38e4bbda590a357e0486bde4
More-Pe <peraltamorena.va@gmail.com>
2025-10-06 23:30:22 +0200

    fix(CorsConfig, SecurityConfig): add front url

--- src/main/java/com/more_than_code/go_con_coche/config/CorsConfig.java
+++ src/main/java/com/more_than_code/go_con_coche/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5175")
+                        .allowedOrigins("http://localhost:5173", "https://More-ThanCode.github.io/GoConcoche-Front")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
